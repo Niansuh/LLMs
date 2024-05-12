6222c53de8286f5d5b2972e6dafe3f108f546597
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-05-12 19:41:34 +0500

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          <a href="https://bit.ly/3UB3z0R">Support me</a>
+          NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Clicking on "Support me" will redirect you to advertisements, allowing me to earn. Your support is greatly appreciated.
+          Build your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
