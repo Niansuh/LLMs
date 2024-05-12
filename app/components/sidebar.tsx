17e71e2b6bbba739ffad29a204ea1d3ef17a4277
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-05-12 19:50:57 +0500

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           <a href="https://bit.ly/3UB3z0R">Support me</a>
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Clicking on "Support me" will redirect you to advertisements, allowing me to earn. Your support is greatly appreciated.
+          Clicking on Support me will redirect you to advertisements, allowing me to earn. Your support is greatly appreciated.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
