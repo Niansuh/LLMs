fb3c9401f9961a0054e75cb5603cc1f34425ff2b
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-05-13 15:21:49 +0500

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           Niansuh
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          AI assistant.
+          AI assistant
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
