420ff422ea85e773ba4caaf30ca45f6c55b1e0a2
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-05-25 18:41:30 +0500

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,11 +1,11 @@
 export const OWNER = "Niansuh";
-export const REPO = "LLMs";
+export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const UPDATE_URL = `https://chatgptbots.net`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const FETCH_TAG_URL = `https://api.github.com/repos/Yidadaa/ChatGPT-Next-Web/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export const DEFAULT_API_HOST = "https://api.nextchat.dev";
